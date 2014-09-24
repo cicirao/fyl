95a37bf7744c1ebf5ee0195b1386aa85c60a5f78
Xi Rao <cicirao.hust@gmail.com>
2014-09-24 15:36:31 -0500

    add date

---
+++ js/tweets.js
@@ -0,0 +1,52 @@
+var tweets = [
+  {
+  	"id": 1,
+  	"summary": "#StopGovtAbuse week continues w/ action to rein in red tape that’s making it harder for #smallbiz to create #jobs: http://t.co/Ph2Mqqsk1T",
+  	"author": "SpeakerBoehner",
+  	"date": "Feb. 27, 2014, 9:26 a.m"},
+  {
+  	"id": 2,
+  	"summary": "116,000+ have spoken out on proposed rules to allow #IRS to stifle speech. Have you? Add your voice before midnight: http://t.co/oofm6SgItK",
+  	"author": "SenTedCruz ",
+  	"date": "Feb. 27, 2014, 9:22 a.m"},
+  {
+  	"id": 3,
+  	"summary": "Connie &amp; I send our prayers on 2nd anniversary of tragic shooting at Chardon High School. Today remember to be kind: http://t.co/e9Jhp26i7W",
+  	"author": "SenSherrodBrown",
+  	"date": "Feb. 27, 2014, 9:20 a.m"},
+  {
+  	"id": 4,
+  	"summary": "“I may not always agree with what you say, but I’ll defend to the death your right to say it.” – not the @FCC http://t.co/CbBoySVVEE",
+  	"author": "SenToomey",
+  	"date": "Feb. 27, 2014, 9:19 a.m"},
+  {
+  	"id": 5,
+  	"summary": "RT @SenateBanking: Today Chairman @SenJohnsonSD will hold a hearing w/@FedReserve Chair Yellen on monetary policy. Live here @ 10AM: http:/…",
+  	"author": "SenJohnsonSD ",
+  	"date": "Feb. 27, 2014, 9:14 a.m"},
+  {
+  	"id": 6,
+  	"summary": "#StopGovtAbuse week continues w/ action to rein in red tape that’s making it harder for #smallbiz to create #jobs: http://t.co/Ph2Mqqsk1T",
+  	"author": "SpeakerBoehner",
+  	"date": "Feb. 27, 2014, 9:26 a.m"},
+  {
+  	"id": 7,
+  	"summary": "116,000+ have spoken out on proposed rules to allow #IRS to stifle speech. Have you? Add your voice before midnight: http://t.co/oofm6SgItK",
+  	"author": "SenTedCruz ",
+  	"date": "Feb. 27, 2014, 9:22 a.m"},
+  {
+  	"id": 8,
+  	"summary": "Connie &amp; I send our prayers on 2nd anniversary of tragic shooting at Chardon High School. Today remember to be kind: http://t.co/e9Jhp26i7W",
+  	"author": "SenSherrodBrown",
+  	"date": "Feb. 27, 2014, 9:20 a.m"},
+  {
+  	"id": 9,
+  	"summary": "“I may not always agree with what you say, but I’ll defend to the death your right to say it.” – not the @FCC http://t.co/CbBoySVVEE",
+  	"author": "SenToomey",
+  	"date": "Feb. 27, 2014, 9:19 a.m"},
+  {
+  	"id": 10,
+  	"summary": "RT @SenateBanking: Today Chairman @SenJohnsonSD will hold a hearing w/@FedReserve Chair Yellen on monetary policy. Live here @ 10AM: http:/…",
+  	"author": "SenJohnsonSD ",
+  	"date": "Feb. 27, 2014, 9:14 a.m"}
+];
